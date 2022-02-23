c10b6ab4c81293b9c3a623ec5314b4f13947d013
geonil2 <kji8922@naver.com>
2022-02-23 22:51:10 +0900

    update validation

--- Dwitter/server/router/tweets.js
+++ Dwitter/server/router/tweets.js
@@ -1,10 +1,20 @@
 import express from "express";
 import 'express-async-errors';
+import { body } from "express-validator";
 import * as tweetController from '../controller/tweet.js';
+import { validate } from "../middleware/validator.js";
 
-
+// validation
+// sanitization
 const router = express.Router();
 
+const validateTweet = [
+    body('text')
+    .trim()
+    .isLength({ min : 3 })
+    .withMessage('text should be at least 3 characters'),
+    validate
+]
 // get /tweets
 // get /tweets?username=?
 router.get('/', tweetController.getTweets);
@@ -13,10 +23,10 @@ router.get('/', tweetController.getTweets);
 router.get('/:id', tweetController.getTweet);
 
 // post /tweets
-router.post('/', tweetController.createTweet);
+router.post('/', validateTweet, tweetController.createTweet);
 
-// put /twwets/:id
-router.put('/:id', tweetController.updateTweet);
+// put /tweets/:id
+router.put('/:id', validateTweet, tweetController.updateTweet);
 
 // delete /tweets/:id
 router.delete('/:id', tweetController.deleteTweet);
--- validation/app.js
+++ validation/app.js
@@ -4,29 +4,31 @@ import { body, param, validationResult } from "express-validator";
 const app = express();
 app.use(express.json());
 
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if(errors.isEmpty()) {
+        return next();
+    }
+    return res.status(400).json({ message: errors.array()[0].msg });
+}
 app.post('/users', 
     [
-        body('name').isLength({ min: 2 }).withMessage('이름을 2글자 이상 입력해주세요.'),
+        body('name').trim().isLength({ min: 2 }).withMessage('이름을 2글자 이상 입력해주세요.'),
         body('age').notEmpty().isInt().withMessage('숫자를 입력해주세요.'),
+        body('email').isEmail().withMessage('이메일 입력해요').normalizeEmail(),
         body('job.name').notEmpty(),
+        validate,
     ],
     (req, res, next) => {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()) {
-            res.status(400).json({message: errors.array() });
-        }
+        
         console.log(req.body);
         res.sendStatus(201);
     }
 );
 
 app.get('/:email', 
-    param('email').isEmail().withMessage('이메일 입력해주세요.'),
+    [param('email').isEmail().withMessage('이메일 입력해주세요.'), validate],
     (req, res, next) => {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()) {
-            return res.status(400).json({ message: errors.array() });
-        }
         res.send('good~');
     }
 );
